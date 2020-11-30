19eafcbf1e98304227fa611a38faa2136ca6c0bd
jesuskata <rzarate.alex@gmail.com>
2020-11-30 15:28:03 -0600

    feat(subscription): 🎸 config the subscription type and count ex

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-shadow */
 // Dependencies
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 
 // DB
 import { db } from './db';
@@ -8,21 +8,26 @@ import { db } from './db';
 // Resolvers
 import { Query } from './resolvers/Query';
 import { Mutation } from './resolvers/Mutation';
+import { Subscription } from './resolvers/Subscription';
 import { User } from './resolvers/User';
 import { Post } from './resolvers/Post';
 import { Comment } from './resolvers/Comment';
+
+const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers: {
     Query,
     Mutation,
+    Subscription,
     User,
     Post,
     Comment
   },
   context: {
-    db
+    db,
+    pubsub
   }
 });
 
---
+++ src/resolvers/Subscription.js
@@ -0,0 +1,16 @@
+export const Subscription = {
+  count: {
+    subscribe(parent, args, { pubsub }, info) {
+      let count = 0;
+
+      setInterval(() => {
+        count += 1;
+        pubsub.publish('count', {
+          count
+        });
+      }, 1000);
+
+      return pubsub.asyncIterator('count');
+    }
+  }
+};
