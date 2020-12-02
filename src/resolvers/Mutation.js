e9b3c0862568d89c5a2fbefe608e715908f2e1c9
jesuskata <rzarate.alex@gmail.com>
2020-12-02 16:19:49 -0600

    feat(subscription): 🎸 add comment subscription for edit/delete

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -175,20 +175,31 @@ export const Mutation = {
 
     db.comments.push(comment);
     pubsub.publish(`comment ${args.data.post}`, {
-      comment
+      comment: {
+        mutation: 'CREATED',
+        data: comment
+      }
     });
     return comment;
   },
-  deleteComment(parent, args, { db }, info) {
+  deleteComment(parent, args, { db, pubsub }, info) {
     const commentIndex = db.comments.findIndex((comment) => comment.id === args.id);
     if (commentIndex === -1) {
       throw new Error('Comment not found');
     }
     // Delete the comment found
-    const deletedComment = db.comments.splice(commentIndex, 1);
-    return deletedComment[0];
+    const [deletedComment] = db.comments.splice(commentIndex, 1);
+
+    pubsub.publish(`comment ${deletedComment.post}`, {
+      comment: {
+        mutation: 'DELETED',
+        data: deletedComment
+      }
+    });
+
+    return deletedComment;
   },
-  updateComment(parent, args, { db }, info) {
+  updateComment(parent, args, { db, pubsub }, info) {
     const { id, data } = args;
     const comment = db.comment.find((dbComment) => dbComment.id === id);
 
@@ -199,6 +210,13 @@ export const Mutation = {
     if (typeof data.text === 'string') {
       comment.text = data.text;
     }
+
+    pubsub.publish(`comment ${comment.id}`, {
+      comment: {
+        mutation: 'UPDATED',
+        data: comment
+      }
+    });
 
     return comment;
   }
